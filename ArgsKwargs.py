798617f8a7c7e373498804accf8f1ad3b1426137
Yuliya Lyubykh <kellyb4lyv@gmail.com>
2024-12-14 12:34:42 +0600

    Принять и вывести аргументы и вывести как словарь

---
+++ ArgsKwargs.py
@@ -0,0 +1,9 @@
+def process_args(*args, **kwargs):
+    result = {}
+    for i, arg in enumerate(args, 1):
+        result[f'positional_{i}'] = arg
+
+    for key, value in kwargs.items():
+        result[key] = value
+
+    return result
